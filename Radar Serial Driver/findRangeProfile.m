32e03ffc2d3a11943a45c5e6af7b52d2c0ef9390
Carl Stahoviak <cast9875@colorado.edu>
2019-11-21 12:43:13 -0700

    moved code from ARPG to this repo; removed unrelated dirs

--- Radar Serial Driver/findRangeProfile.m
+++
@@ -1,9 +0,0 @@
-function [tag, len, PAYLOAD, addToOffset] = findRangeProfile(offset, D, numRangeBins)
-    
-    tag     = typecast(uint8(D(offset    : offset + 3)), 'uint32');
-    len     = typecast(uint8(D(offset + 4: offset + 7)), 'uint32');
-    PAYLOAD = D(offset + 8 : offset + 7 + numRangeBins * 2);
-    
-    addToOffset = len;
-    
-end
